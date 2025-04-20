9942dc9a3bafd1f80efddcc10f1ba58016c2cca8
Wavpro <albin.hedwall@gmail.com>
2025-04-20 21:59:22 +0200

    Clean slate

--- app/participants/add/actions.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { createSeatMapKey } from "@/lib/util";
-
-export async function getSeatmapAccessKey(ssn: string) {
-    // change this to be less predictable
-    return createSeatMapKey(ssn);
-}
