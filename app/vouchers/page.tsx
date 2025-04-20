9fc3e940eeccb170bfef848b8c212b6922f44d9d
Wavpro <albin.hedwall@gmail.com>
2025-04-20 21:54:42 +0200

    Clean start

--- app/vouchers/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <div>
-            <h1 className="text-6xl mb-8 font-bold select-none">KASSA</h1>
-        </div>
-    );
-}
--- lib/components/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export default function Loading() {
-    return <FontAwesomeIcon icon={faSpinner} className="loading" />;
-}
